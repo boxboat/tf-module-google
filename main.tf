4611b49939d69dc9c3a096d65f303ef1326e52a6
Bryton Hall <email@bryton.io>
2019-11-14 22:50:58 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+variable "credentials" {}
+variable "project" {}
+variable "region" {}
+variable "zone" {}
+variable "name" {}
+variable "image" {}
+variable "disk" {}
+variable "public_key" {}
+
+provider "google" {
+  credentials = "${var.credentials}"
+  project     = "${var.project}"
+  region      = "${var.region}"
+  zone        = "${var.zone}"
+}
+
+resource "google_compute_instance" "instance" {
+  name         = "${var.name}"
+  machine_type = "n1-standard-1"
+  tags         = ["hobbyfarm"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${var.public_key}"
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {
+      // Ephemeral IP - leaving this block empty will generate a new external IP and assign it to the machine
+    }
+  }
+
+  initialize_params {
+    size = "${var.disk}"
+  }
+}
+
+output "private_ip" {
+  value = "${google_compute_instance.instance.nat_ip}"
+}
+
+output "public_ip" {
+   value = ["${google_compute_instance.instance.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+output "hostname" {
+  value = "${google_compute_instance.instance.hostname}"
+}
